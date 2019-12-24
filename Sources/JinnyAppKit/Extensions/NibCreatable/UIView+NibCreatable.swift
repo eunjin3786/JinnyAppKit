bbf81992e1218bf8a0a204e196a778c3b550348a
eunjin <eunjin3786@naver.com>
2019-12-24 15:55:11 +0900

    NibCreatable

---
+++ Sources/JinnyAppKit/Extensions/NibCreatable/UIView+NibCreatable.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// extensions 내의 override를 허용하려면 @objc 키워드 필요함.
+
+extension UIView: NibCreatable {
+    public final class var nib: UINib {
+        let nibName = Self.nibName
+        let nibBundle = Self.nibBundle
+        return UINib(nibName: nibName, bundle: nibBundle)
+    }
+    
+    @objc
+    public class var nibBundle: Bundle? {
+        return Bundle(for: self)
+    }
+    
+    @objc
+    public class var nibName: String {
+        return "\(self)"
+    }
+    
+    public final class func instanceFromNib() -> Self {
+        return nib.instantiate(withOwner: nil, options: nil).last as! Self
+    }
+}
+
+
+// Self와 self의 차이: https://www.hackingwithswift.com/example-code/language/self-vs-self-whats-the-difference
