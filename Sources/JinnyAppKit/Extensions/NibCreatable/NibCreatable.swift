e122d4dbe41337afa23debf75df473b9bfe1ac61
eunjin <eunjin3786@naver.com>
2019-12-24 16:26:04 +0900

    StoryboardCreatable 추가

--- Sources/JinnyAppKit/Extensions/NibCreatable/NibCreatable.swift
+++ Sources/JinnyAppKit/Extensions/NibCreatable/NibCreatable.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-public protocol NibCreatable {
+public protocol NibCreatable: class {
     static var nib: UINib { get }
     static var nibBundle: Bundle? { get }
     static var nibName: String { get }
---
+++ Sources/JinnyAppKit/Extensions/StoryboardCreatable/StoryboardCreatable.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+public protocol StoryboardCreatable: class {
+  static var storyboard: UIStoryboard { get }
+  static var storyboardBundle: Bundle? { get }
+  static var storyboardIdentifier: String { get }
+  static var storyboardName: String { get }
+  
+  static func instanceFromStoryboard() -> Self
+}
+
